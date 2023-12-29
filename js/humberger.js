924618a3526a5007133334c1b64f727abb41b943
TakiKun78 <sanjayaridho043@gmail>
2023-12-30 00:36:17 +0800

    Membuat form mengarah ke spreadsheets dan menambahkan sedikit alert saat mengklik button

--- js/humberger.js
+++ js/humberger.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Menu toggle
   const menu = document.querySelector('.menu');
   const humbergerMenu = document.querySelector('.humberger-menu');
   const iconBars = document.querySelector('.icon-bars');
@@ -17,16 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
       iconClose.style.display = 'inline';
     }
   }
-});
 
-document.addEventListener('DOMContentLoaded', function() {
+  // Menu button click
   const menuButton = document.querySelector('.humberger-menu');
-  const menu = document.querySelector('.menu');
+  const menuElement = document.querySelector('.menu');
 
   menuButton.addEventListener('click', function() {
-    menu.classList.toggle('active');
+    menuElement.classList.toggle('active');
   });
 
+  // Smooth scroll for menu links
   const navLinks = document.querySelectorAll('.menu a');
 
   navLinks.forEach(function(link) {
@@ -41,21 +42,59 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
-});
 
-anime({
-  targets: '.underline-link',
-  translateX: 600,
-  autoplay: true,
-  easing: 'easeInOutSine'
-});
+  // Anime library animation
+  anime({
+    targets: '.underline-link',
+    translateX: 600,
+    autoplay: true,
+    easing: 'easeInOutSine'
+  });
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzIyb1ouWooGQOucALsJosRXbU1CnjVVJowkIMd74chOm8M2ogi7KU5tryiQIadA4s/exec';
-const form = document.forms['ridho-form-contact'];
+  // Form submission
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzIyb1ouWooGQOucALsJosRXbU1CnjVVJowkIMd74chOm8M2ogi7KU5tryiQIadA4s/exec';
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message));
+  // Form submission with SweetAlert
+  const formWithAlert = document.getElementById('ridho-form-contact');
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const commentTextarea = document.getElementById('comment');
+
+  formWithAlert.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(formWithAlert) })
+      .then(response => {
+        if (response.ok) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Pesan berhasil terkirim!',
+            showConfirmButton: false,
+            timer: 1500,
+            customClass: {
+              popup: 'custom-alert-popup',
+              content: 'custom-alert-content',
+              title: 'custom-alert-title'
+            }
+          }).then(() => {
+            // Clear form after successful submission
+            nameInput.value = '';
+            emailInput.value = '';
+            commentTextarea.value = '';
+          });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Gagal mengirim pesan. Silakan coba lagi.'
+          });
+        }
+      })
+      .catch(error => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Gagal mengirim pesan. Silakan coba lagi.'
+        });
+        console.error('Error!', error.message);
+      });
+  });
 });
